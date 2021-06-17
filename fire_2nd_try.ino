c3dbc28c4a2c76393fe58a76c1c5c834b00312a7
Ashutosh Vimal <68318973+ashutoshvimal@users.noreply.github.com>
2021-06-17 16:47:20 +0530

    Update fire_2nd_try.ino

--- fire_2nd_try.ino
+++ fire_2nd_try.ino
@@ -3,7 +3,7 @@ const int threshold=200;// sets threshold value for flame sensor
 void setup() 
 {
 Serial.begin(9600);
-//pinMode(13,OUTPUT);
+pinMode(13,OUTPUT);
 pinMode(A2,INPUT);
 pinMode(8,OUTPUT);
 }
@@ -12,11 +12,12 @@ void loop()
  
 int flamesensvalue=analogRead(A2); // reads analog data from flame sensor
 if (flamesensvalue<=threshold) { // compares reading from flame sensor with the threshold value
+digitalWrite(13,HIGH);
 digitalWrite(8,HIGH); //turns on led and buzzer
 //delay (1000);
 //digitalWrite(13,LOW);
 //delay (1000);
-tone(8,500);
+tone(8,1000);
 delay(500); //stops program for 0.5 second
 }
 else
@@ -24,5 +25,4 @@ else
 digitalWrite(8,LOW); //turns led off led and buzzer
 noTone(8);
 }
-
 }
