58af67d1f39943f1ac6f269f6e5b47e09a57018f
amarnathkr <amarkr697@gmail.com>
2021-06-17 16:44:08 +0530

    Add files via upload

---
+++ fire_2nd_try.ino
@@ -0,0 +1,28 @@
+const int threshold=200;// sets threshold value for flame sensor
+//int flamesensvalue=0; // initialize flamesensor reading
+void setup() 
+{
+Serial.begin(9600);
+//pinMode(13,OUTPUT);
+pinMode(A2,INPUT);
+pinMode(8,OUTPUT);
+}
+void loop() 
+{
+ 
+int flamesensvalue=analogRead(A2); // reads analog data from flame sensor
+if (flamesensvalue<=threshold) { // compares reading from flame sensor with the threshold value
+digitalWrite(8,HIGH); //turns on led and buzzer
+//delay (1000);
+//digitalWrite(13,LOW);
+//delay (1000);
+tone(8,500);
+delay(500); //stops program for 0.5 second
+}
+else
+{
+digitalWrite(8,LOW); //turns led off led and buzzer
+noTone(8);
+}
+
+}
